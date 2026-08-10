b6a5ea9a86d186661596338e7aeccef59d7ebf9c
angelmorel049-cmyk <angelmorel049@gmail.com>
2026-08-10 14:10:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,63 @@
 const http = require("http");
 
 const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
+
+const rooms = {};
+
+function generateCode() {
+    let code;
+
+    do {
+        code = Math.floor(1000 + Math.random() * 9000).toString();
+    } while (rooms[code]);
+
+    return code;
+}
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        "Content-Type": "text/plain"
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
     });
 
-    res.end("Scary Horror Game Server is online!");
+    if (req.url === "/") {
+        res.end(JSON.stringify({
+            online: true,
+            message: "Scary Horror Game Server is online!"
+        }));
+        return;
+    }
+
+    if (req.url === "/create") {
+        const code = generateCode();
+
+        rooms[code] = {
+            players: 1
+        };
+
+        res.end(JSON.stringify({
+            success: true,
+            code: code
+        }));
+
+        console.log("Room created:", code);
+        return;
+    }
+
+    if (req.url === "/rooms") {
+        res.end(JSON.stringify({
+            rooms: rooms
+        }));
+        return;
+    }
+
+    res.end(JSON.stringify({
+        success: false,
+        error: "Unknown request"
+    }));
 });
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(PORT, HOST, () => {
+    console.log(`Server running on ${HOST}:${PORT}`);
 });
